f41a7008f40e8c62e212d595dbdc5a86e5e5af3e
carraes <carraeshb@gmail.com>
2025-09-26 23:30:59 -0300

    feat: add multicursor

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,4 @@
 vim.pack.add({
-
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1") },
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
@@ -13,6 +12,7 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/mfussenegger/nvim-lint" },
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	{ src = "https://github.com/jake-stewart/multicursor.nvim" },
 	{ src = "https://github.com/christoomey/vim-tmux-navigator" },
 }, { load = true })
 
@@ -147,6 +147,111 @@ require("gitsigns").setup({
 		map("n", ";q", gs.toggle_deleted)
 	end,
 })
+
+-- Multicursor
+local mc = require("multicursor-nvim")
+mc.setup({})
+
+local bind = vim.keymap.set
+-- Add or skip cursor above/below the main cursor.
+bind({ "n", "v" }, "<up>", function()
+	mc.lineAddCursor(-1)
+end)
+bind({ "n", "v" }, "<down>", function()
+	mc.lineAddCursor(1)
+end)
+bind({ "n", "v" }, ";k", function()
+	mc.lineSkipCursor(-1)
+end)
+bind({ "n", "v" }, ";j", function()
+	mc.lineSkipCursor(1)
+end)
+
+-- Add or skip adding a new cursor by matching the current word/selection
+bind("v", "<leader>n", function()
+	mc.matchAddCursor(1)
+end)
+bind({ "n", "v" }, "<leader>s", function()
+	mc.matchSkipCursor(1)
+end)
+bind({ "n", "v" }, "<leader>N", function()
+	mc.matchAddCursor(-1)
+end)
+bind({ "n", "v" }, "<leader>S", function()
+	mc.matchSkipCursor(-1)
+end)
+
+-- Add a cursor and jump to the next word under cursor.
+bind("v", "<c-n>", function()
+	mc.addCursor("*")
+end)
+
+-- Jump to the next word under cursor but do not add a cursor.
+bind("v", "<c-s>", function()
+	mc.skipCursor("*")
+end)
+
+-- Rotate the main cursor.
+bind({ "n", "v" }, "<left>", mc.nextCursor)
+bind({ "n", "v" }, "<right>", mc.prevCursor)
+
+-- Delete the main cursor.
+bind("v", "<leader>x", mc.deleteCursor)
+
+-- Add and remove cursors with control + left click.
+bind("n", "<c-leftmouse>", mc.handleMouse)
+
+bind({ "n", "v" }, "<c-q>", function()
+	if mc.cursorsEnabled() then
+		-- Stop other cursors from moving.
+		-- This allows you to reposition the main cursor.
+		mc.disableCursors()
+	else
+		mc.addCursor()
+	end
+end)
+
+-- clone every cursor and disable the originals
+bind({ "n", "v" }, "<leader><c-q>", mc.duplicateCursors)
+
+bind("n", "<esc>", function()
+	if not mc.cursorsEnabled() then
+		mc.enableCursors()
+	elseif mc.hasCursors() then
+		mc.clearCursors()
+	else
+		-- Default <esc> handler.
+	end
+end)
+
+-- Align cursor columns.
+bind("v", "<leader>a", mc.alignCursors)
+
+-- Split visual selections by regex.
+bind("v", "S", mc.splitCursors)
+
+-- Append/insert for each line of visual selections.
+bind("v", "I", mc.insertVisual)
+bind("v", "A", mc.appendVisual)
+
+-- match new cursors within visual selections by regex.
+bind("v", "M", mc.matchCursors)
+
+-- Rotate visual selection contents.
+bind("v", "<leader>t", function()
+	mc.transposeCursors(1)
+end)
+bind("v", "<leader>T", function()
+	mc.transposeCursors(-1)
+end)
+
+-- Customize how cursors look.
+vim.api.nvim_set_hl(0, "MultiCursorCursor", { link = "Cursor" })
+vim.api.nvim_set_hl(0, "MultiCursorVisual", { link = "Visual" })
+vim.api.nvim_set_hl(0, "MultiCursorSign", { link = "SignColumn" })
+vim.api.nvim_set_hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
+vim.api.nvim_set_hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+vim.api.nvim_set_hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
 
 -- Mini
 require("mini.ai").setup()
